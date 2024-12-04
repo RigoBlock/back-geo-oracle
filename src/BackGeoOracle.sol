5d08f754cede41c7f62501587469b46aef827b8b
Gabriele Rigo <gabriele.rigo@gmail.com>
2024-12-04 18:16:05 +0100

    correctly execute backrun

--- src/BackGeoOracle.sol
+++ src/BackGeoOracle.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.26;
 import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
 
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {TickMath} from "v4-core/src/libraries/TickMath.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
@@ -40,14 +41,6 @@ contract BackGroOracle is BaseHook {
     /// @notice The current observation array state for the given pool ID
     mapping(PoolId => ObservationState) public states;
 
-    // NOTE: ---------------------------------------------------------
-    // state variables should typically be unique to a pool
-    // a single hook contract should be able to service multiple pools
-    // ---------------------------------------------------------------
-
-    mapping(PoolId => uint256 count) public beforeSwapCount;
-    mapping(PoolId => uint256 count) public afterSwapCount;
-
     constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
 
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
@@ -69,7 +62,6 @@ contract BackGroOracle is BaseHook {
         });
     }
 
-    // TODO: check whether we want an oracle to exist where token0 is base currency only
     function beforeInitialize(address, PoolKey calldata key, uint160, bytes calldata)
         external
         view
@@ -79,8 +71,9 @@ contract BackGroOracle is BaseHook {
     {
         // This is to limit the fragmentation of pools using this oracle hook. In other words,
         // there may only be one pool per pair of tokens that use this hook. The tick spacing is set to the maximum
-        // because we only allow max range liquidity in this pool.
-        if (key.fee != 0 || key.tickSpacing != manager.MAX_TICK_SPACING()) revert OnlyOneOraclePoolAllowed();
+        // because we only allow max range liquidity in this pool. The currency0 must be base currency to prevent
+        // rogue oracles.
+        if (key.fee != 0 || key.tickSpacing != TickMath.MAX_TICK_SPACING || key.currency0 !== address(0)) revert OnlyOneOraclePoolAllowed();
         return GeomeanOracle.beforeInitialize.selector;
     }
 
@@ -122,24 +115,14 @@ contract BackGroOracle is BaseHook {
         return BaseHook.beforeAddLiquidity.selector;
     }
 
-    // TODO: is onlyByManager modifier necessary in these methods?
     function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
         external
         override
         onlyByManager
         returns (bytes4, BeforeSwapDelta, uint24)
     {
-        // TODO: update should store min tick, max tick observed in a block. We cannot use the swap tick delta as a swap can be
-        // divided into multiple ones. Should use min and max to verify what the tick delta is, and what the after swap fee will be.
         _updatePool(key);
         return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
-    }
-
-    struct CallbackData {
-        address sender;
-        PoolKey key;
-        IPoolManager.SwapParams params;
-        uint256 backrunAmount;
     }
 
     function afterSwap(
@@ -149,50 +132,48 @@ contract BackGroOracle is BaseHook {
         BalanceDelta delta,
         bytes calldata
     ) external override onlyByManager returns (bytes4, int128) {
-        // fee will be in the unspecified token of the swap
-        bool isCurrency0Specified = (params.amountSpecified < 0 == params.zeroForOne);
+        int128 feeAmount;
 
-        (Currency currencyUnspecified, int128 amountUnspecified) =
-            (isCurrency0Specified) ? (key.currency1, delta.amount1()) : (key.currency0, delta.amount0());
-
-        // if exactOutput swap, get the absolute output amount
-        if (amountUnspecified < 0) amountUnspecified = -amountUnspecified;
-
-        // TODO: apply range-based fee
-        uint256 rangeBasedFee;
-        uint256 feeAmount = uint256(int256(amountUnspecified)).mulWadDown(rangeBasedFee);
-
-        // mint ERC6909 as its cheaper than ERC20 transfer
-        poolManager.mint(address(this), CurrencyLibrary.toId(currencyUnspecified), feeAmount);
-
-        // backrun swap
-        // TODO: correctly detect undetected amount token, as we want to make sure we are able to take the undetermined amount
-        //  i.e. we are selling in the undetermined amount
-        if (callBackData.params.zeroForOne) {
-            //Flash swap token1 for token0. We do not have to worry about front running, as we are backrunning
-            IPoolManager.SwapParams memory token1to0 = IPoolManager.SwapParams({
-                zeroForOne: false,
-                amountSpecified: feeAmount,
-                sqrtPriceLimitX96: 0
-            });
-            BalanceDelta delta = poolManager.swap(data.key, token1to0);
-            // TODO: repay delta to sender
-            return abi.encode(delta);
-        } else {
-            IPoolManager.SwapParams memory token0to1 = IPoolManager.SwapParams({
-                zeroForOne: true,
-                amountSpecified: feeAmount,
-                sqrtPriceLimitX96: 0
-            });
-            BalanceDelta delta = poolManager.swap(data.key, token0to1);
+        if (sender !== address(this)) {
+            feeAmount = abi.decode(poolManager.unlock(abi.encode(key, params, delta)), int128);
         }
-        // technically, as minting the token is a liability, if we return the full amount, we can return delta 0
-        // and avoid minting the nft, so we do not need to transfer back. This means no tax is applied to the caller,
-        // and we should check if this is enough to guarantee the token won't be moved. We should check smaller increments
-        // as an attacker could potentially try to manipulate illiquid tokens quite frequently.
 
-        // at this point, we could repay to the tokens to the user, or should decide how to best allocate extra funds
+        return (BaseHook.afterSwap.selector, feeAmount); //feeAmount.toInt128()
+    }
 
-        return (BaseHook.afterSwap.selector, feeAmount.toInt128());
+    struct CallbackData {
+        PoolKey key;
+        IPoolManager.SwapParams params;
+        BalanceDelta delta;
+    }
+
+    /// @notice prepares the hook for swap on the pool manager
+    /// @dev This call is only callable by this contract via poolManager.unlock
+    function unlockCallback(bytes calldata rawData) external override onlyByManager returns (bytes memory result) {
+        CallbackData memory data = abi.decode(rawData, (CallbackData));
+        (, int24 tick,,) = manager.getSlot0(id);
+        bool shouldBackrun = true;
+
+        // overwrite undefined amount with backrun amount
+        if ((tick - last.prevTick) > 9116 || (tick - last.prevTick) < -9116) {
+            // Full backrun
+        } else if ((tick - last.prevTick) > 4558 || (tick - last.prevTick) < -4558) {
+            data.params.amountSpecified = data.params.amountSpecified * 4 * 1.1;
+        } else if ((tick - last.prevTick) > 410 || (tick - last.prevTick) < -410) {
+            data.params.amountSpecified = data.params.amountSpecified * 4 * 0.9;
+        } else {
+            shouldBackrun = false;
+        }
+
+        BalanceDelta memory delta;
+
+        // early escape if no backrun is necessary, to save gas on small impact swaps
+        if (shouldBackrun) {
+            data.params.zeroForOne = !data.params.zeroForOne;
+            data.params.sqrtPriceLimitX96 = data.params.zeroForOne ? TickMath.MIN_SQRT_PRICE + 1 : TickMath.MAX_SQRT_PRICE - 1;
+            delta = poolManager.swap(data.key, data.params, "");
+        }
+
+        return abi.encode(delta);
     }
 }
